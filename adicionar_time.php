146757cf9d2bafe38b2af03660786459ea4dfcb4
José Vítor Heringer <josevitorheringer@gmail.com>
2017-06-17 01:36:48 -0300

    Resaolvido erro ao enviar os times para o arquivo

--- adicionar_time.php
+++ adicionar_time.php
@@ -6,54 +6,51 @@
 	<link rel="stylesheet" type="text/css" href="./css/estiloadctime.css">
 </head>
 <body>
-	<script type="text/javascript">
-		
-		alert("Olá, se você não for Nattan Duarte ou José Vitor Heringer, clique na setinha para a esquerda no canto superior esquerdo do seu navegador. Se você for José ou Nattan, primeiro, você é lindo, cara, agora vamos consertar essa porra, beijos");
 
-	</script>
 
-	<video width="100%" height="100%" autoplay loop muted>
-		<source src="esquiloladrao.mp4" type="video/mp4"/>
-	</video>
 
-	<div class="tudao">
-	<form method="post">
+	<div class="">
+	<form action="adicionar_time.php" method="get">
 		<h1>Adicione seu time!</h1>
 		<br/>
 		Nome do time:<input type="text" name="ntime" value=""/>
 		<br/>
 		<input type="submit"/>
 	</form>
-		<?php
+</div>
 
+<?php
 
-			$ntime=$_POST['ntime'];
+	
+	if( !array_key_exists( 'ntime', $_GET ) ) return;
 
+	$caminhoArquivo = "./documentos/times.txt";
 
-			$ntime = mb_strtoupper($ntime);
+	$ntime=$_GET['ntime'];
+	$arquivo = '';
 
-			$handle=fopen("./documentos/times.txt", "r+");
+	$ntime =  mb_strtoupper($ntime);
+	$handle=fopen($caminhoArquivo, "r+");
 
-			while (!feof($handle)) {
-					
-							
-				if ($ntime!=fgets($handle)) {
-					
-					fwrite($handle, $ntime.'\n \t'.PHP_EOL);
-					echo "Time inscrito com sucesso!";
-					break;
-				}
-				else{
-				
-					?>
-					<p>Aparentemente o time que você inseriu já está disponível. Corra e acompanhe os resultados <a href="">na tabela</a></p>
-					<?php
-					break;
-				}
-			}	
-			fclose($handle);
-	?>
-	</div>
+	while(!feof($handle)){
+
+		$linha= trim( fgets($handle) );
+		$b = trim( $ntime . PHP_EOL );
+
+		if( $b ==$linha){
+			die("<p class='erro-adicao'>O time $ntime já está cadastrado, confira a nossa <a href='./documentos/times.txt'>tabela</a></p>");
+		}
+
+		$arquivo .= $linha . PHP_EOL;
+	}
+
+	fclose($handle);
+
+	$a = $ntime . PHP_EOL . $arquivo;
+	file_put_contents($caminhoArquivo, $a );
+	
+	echo "<p class='sucesso-adicao'>Time $ntime inscrito com sucesso!</p>";
+?>
 
 </body>
 
