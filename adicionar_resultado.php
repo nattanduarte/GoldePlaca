10d65e7956b43a1a7c6833536e40de66a5ca463a
Nattan Duarte <nattanfd@gmail.com>
2017-06-19 17:19:50 -0300

    Versão 1.5
    - Resultado adicionado

---
+++ adicionar_resultado.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html>
+<head>
+	<title>Gerenciador de Campeonatos</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/">
+</head>
+<body>
+
+
+
+	<div class="">
+	<form action="adicionar_resultado.php" method="get">
+		<h1>Adicione o resultado!</h1>
+		<br/>
+		Nome do time mandante:<input type="text" name="timem" value=""/>
+		<br/>
+		Placar do time mandante:<input type="number" name="resultado_timem" value=""/>
+		<br/>
+		Nome do time mandante:<input type="text" name="timev" value=""/>
+		<br/>
+		Placar do time mandante:<input type="number" name="resultado_timev" value=""/>
+		<br/>
+		<input type="submit"/>
+	</form>
+</div>
+
+<?php
+
+	
+	if( !array_key_exists( 'timem', $_GET ) ) return;
+	if( !array_key_exists( 'timev', $_GET ) ) return;
+	if( !array_key_exists( 'resultado_timem', $_GET ) ) return;
+	if( !array_key_exists( 'resultado_timev', $_GET ) ) return;
+	
+
+	$caminhoArquivo = "./documentos/resultados.txt";
+	$caminhoArquivoTimes = "./documentos/times.txt";
+
+	$timem=$_GET['timem'];
+	$resultado_timem=$_GET['resultado_timem'];
+	$timev=$_GET['timev'];
+	$resultado_timev=$_GET['resultado_timev'];
+	
+	$arquivo = '';
+
+	$timem =  mb_strtoupper($timem);
+	$timev =  mb_strtoupper($timev);
+	
+	$handle_times=fopen($caminhoArquivoTimes, "r");
+	$handle_resultado=fopen($caminhoArquivo, "r+");
+	
+	$b = trim( $timem . PHP_EOL );
+	$c = trim( $timev . PHP_EOL );
+
+	while(!feof($handle_times)){
+
+		$linha = trim( fgets($handle_times) );
+		
+
+		if( $timem == $linha ){
+			rewind ( $handle_times );
+			
+			while(!feof($handle_times)){
+				
+				$linha = trim( fgets($handle_times) );
+				if($c == $linha){
+					
+						while(!feof($handle_resultado)){
+							$linha = trim( fgets($handle_resultado) );
+								$arquivo .= $linha . PHP_EOL;	
+							$resultado=$timem .';'. $timev.';'.$resultado_timem.';'.$resultado_timev;
+							if($linha==$resultado){
+								
+								echo "O resultado deste jogo já foi adicionado";	
+								die;
+							}
+						
+						}			
+					
+					$a = $timem .';'. $timev.';'.$resultado_timem.';'.$resultado_timev. PHP_EOL . $arquivo;
+			
+					file_put_contents($caminhoArquivo, $a );
+					
+					die("Resultado adicionado com sucesso!");
+				}
+			}
+			
+		}
+		
+	}
+	fclose($handle_times);
+	fclose($handle_resultado);
+	echo "Um dos times não está cadastrado";
+?>
+
+</body>
+
+</html>
